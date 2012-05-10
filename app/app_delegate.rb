fb94904edcbc3f3a289b8a9e0d2ce87af39fe667
Richard Owens <richardowens@new-host-3.home>
2012-05-10 06:06:52 -0400

    readme

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,14 +2,15 @@ class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
+
     listViewController = ListViewController.alloc.init
     detailsViewController = DetailsViewController.alloc.init
     splitViewController = UISplitViewController.alloc.init    
-    splitViewController.viewControllers = [listViewController, detailsNav]
     splitViewController.delegate = detailsViewController
     listViewController.delegate = detailsViewController    
     detailsNav = UINavigationController.alloc.initWithRootViewController(detailsViewController)    
-
+    splitViewController.viewControllers = [listViewController, detailsNav]
+    
     @window.rootViewController = splitViewController
     @window.makeKeyAndVisible;
     true
